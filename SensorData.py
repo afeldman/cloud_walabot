9bc2a69fc8ba0b1c0eb2cecfb53fda2764163961
AntonFeldmann <anton.feldmann@gmail.com>
2016-08-23 23:10:48 +0200

    make jason of target coordinate

--- SensorData.py
+++ SensorData.py
@@ -1,10 +1,5 @@
 #!/user/bin/python
-
 import WalabotAPI as wala
-from __future__ import print_function
-from sys import platform
-from os import system
-
 import json
 
 class Serializer(object):
@@ -17,30 +12,47 @@ class Serializer(object):
     def serialize(obj):
         return json.dumps(obj, default=Serializer.encode_obj)
 
-class JTarget(object):
-    def __init__(self, target):
+class TargetPoint:
+    def __init__(self, index, target):
+        self.index = index
         self.x = target.xPosCm
         self.y = target.yPosCm
         self.z = target.zPosCm
-
     def toJson(self):
         return Serializer.serialize(self)
+    def toString(self):
+        return str('Target #{}x:{}y:{}z:{}').format(self.index + 1,
+                self.x, self.y, self.z)
+
+def TargetsToAzure(targets):
+    for i, target in enumerate(targets):
+       t = TargetPoint(i,target)
+       print(t.toJson())
 
 if __name__ == "__main__":
 
+    debug = True
+
     walabot_hub = 'walabot'
 
-    key_name = 'RootManageSharedAccessKey' # SharedAccessKeyName from Azure portal
-    key_value = '' # SharedAccessKey from Azure portal
-    sbs = ServiceBusService(service_namespace,
-                            shared_access_key_name=key_name,
-                            shared_access_key_value=key_value)
+#    key_name = 'RootManageSharedAccessKey' # SharedAccessKeyName from Azure portal
+#    key_value = '' # SharedAccessKey from Azure portal
+#    sbs = ServiceBusService(service_namespace,
+                #            shared_access_key_name=key_name,
+                 #           shared_access_key_value=key_value)
 
-    sbs.create_event_hub(walabot_hub)
+#    sbs.create_event_hub(walabot_hub)
 
     wala.Init()
     wala.SetSettingsFolder()
-    wala.ConnectAny()
+
+    try:
+        wala.ConnectAny()
+    except WalabotError as err:
+        if err.message == wala.WALABOT_INSTRUMENT_NOT_FOUND:
+            print('Please connect your Walabot')
+        else:
+            print(err.message)
 
     wala.SetProfile(wala.PROF_SENSOR)
     wala.SetDynamicImageFilter(wala.FILTER_TYPE_MTI)
@@ -49,12 +61,17 @@ if __name__ == "__main__":
 
     while True:
         wala.Trigger()
+
         targets = wala.GetSensorTargets()
+        if targets:
+            appStatus, calibrationProcess = wala.GetStatus()
 
-        for i, t in enumerate(targets):
-            jtarget = JTarget(t)
-            if debug: print (jtarget.toJson())
-            sbs.send_event(walabot_hub, jtarget.toJson())
+            if debug :print (appStatus)
+            if debug :print (calibrationProcess)
 
-    wlbt.Stop()
-    wlbt.Disconnect()
+            TargetsToAzure(targets)
+        else:
+            print('No Target Detected')
+
+    wala.Stop()
+    wala.Disconnect()
